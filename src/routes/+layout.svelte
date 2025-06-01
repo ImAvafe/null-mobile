aaa93932f4ccc35b6f22b9febc80ae9fbb75592e
Avafe <65048459+ImAvafe@users.noreply.github.com>
2025-05-31 23:21:53 -0400

    Remove bg noise

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,10 +7,3 @@
 <div class="bg-black fixed w-[100%] h-[100%]">
 	{@render children()}
 </div>
-
-<style>
-	div {
-		background-image: url('$lib/img/noise.svg');
-		background-size: 100%;
-	}
-</style>
